5a6170f7f8aba891d2f688008fbd0b6ec12aab43
Sinan Dede <48207026+SinanDede@users.noreply.github.com>
2025-09-27 20:37:02 +0300

    Update tokenizer.py

---
+++ advanced_text_processor/tokenizer.py
@@ -0,0 +1,11 @@
+def tokenize(text):
+    """
+    Tokenizes text into a list of words.
+
+    Args:
+        text (str): Input text.
+
+    Returns:
+        list: List of tokens (words).
+    """
+    return text.split()
