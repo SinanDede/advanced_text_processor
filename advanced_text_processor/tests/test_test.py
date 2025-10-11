214bb38e3d588fa5e357fea1b9576c11d2d23239
Sinan Dede <48207026+SinanDede@users.noreply.github.com>
2025-10-11 18:15:39 +0300

    Update test_test.py

--- advanced_text_processor/tests/test_test.py
+++ advanced_text_processor/tests/test_test.py
@@ -1,1 +1,9 @@
+from advanced_text_processor import clean_text, tokenize, generate_ngrams, vectorize_text
+
+def test_pipeline():
+    text = "Hello hello world!"
+    tokens = tokenize(clean_text(text))
+    ngrams = generate_ngrams(tokens, 2)
+    vec = vectorize_text(ngrams)
+    assert "hello world" in vec
 
