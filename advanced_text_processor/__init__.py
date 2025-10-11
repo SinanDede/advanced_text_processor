612803df7a670aba9eee2de599193ab953c2324a
Sinan Dede <48207026+SinanDede@users.noreply.github.com>
2025-10-11 18:43:04 +0300

    Update __init__.py

--- advanced_text_processor/__init__.py
+++ advanced_text_processor/__init__.py
@@ -9,14 +9,16 @@ This package provides utilities for:
 - Dataset processing
 """
 
+# advanced_text_processor/advanced_text_processor/__init__.py
 from .text_cleaner import clean_text
 from .tokenizer import tokenize
 from .ngrams import generate_ngrams
 from .vectorizer import vectorize_text
 from .dataset import process_text_file, process_dataset
+
 from .data_tools import (
     summarize_table, fuzzy_merge, balanced_split,
-    save_version, load_version, detect_anomalies
+    save_version, load_version, detect_anomalies,
 )
 
 
